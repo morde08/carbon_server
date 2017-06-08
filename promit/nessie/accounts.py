01fda7fa11e6878b0a823b66396cd9841a3a13ce
promitheus7 <thepromit@gmail.com>
2017-06-08 00:51:02 -0400

    purchase material design

--- promit/nessie/accounts.py
+++ promit/nessie/accounts.py
@@ -9,12 +9,15 @@ def main():
     customer_id = '5938555aa73e4942cdafd84a'
     customer_url = 'http://api.reimaginebanking.com/customers/'
     url = customer_url + customer_id + '/accounts?key=' + key
-    result = requests.get(url).json()[0] # returns result as json
-    for key in result:
-        if isinstance(result[key], int):
-            print(key.encode("utf-8"), result[key])
-        else:
-            print(key.encode("utf-8"), result[key].encode("utf-8"))
+    result = requests.get(url).json()
+    print(type(result))
+    print(result)
+    # result = requests.get(url).json()[0] # returns result as json
+    # for key in result:
+    #     if isinstance(result[key], int):
+    #         print(key.encode("utf-8"), result[key])
+    #     else:
+    #         print(key.encode("utf-8"), result[key].encode("utf-8"))
 
 if __name__ == "__main__":
     main()
